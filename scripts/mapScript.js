815f5aac00b0819b2ce47f9681030e419b17345f
a1719756 <johnnobullas@hotmail.com>
2018-04-25 23:27:05 +0930

    Some changes to search and mapSearch

---
+++ scripts/mapScript.js
@@ -0,0 +1,24 @@
+var map = null;
+
+function initMap(){
+  var hotel = {lat: -34.9315857, lng: 138.56997239999998};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 20,
+    center: hotel
+  });
+  var marker = new google.maps.Marker({
+    position: hotel,
+    map: map
+  });
+}
+
+function goToSearch(){
+  document.getElementById('searchBut').onclick = function() {
+    location.href = "search.html";
+  }
+}
+
+function mapSearch(){
+  var searchLocationName = document.getElementById('searchReq').value;
+  console.log(searchLocationName);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,6 @@
 var peopleCount = 0;
 var setLocation = "Adelaide";
 var roomChoice;
-var map = null;
-
-function initMap(){
-  var hotel = {lat: -34.9315857, lng: 138.56997239999998};
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 20,
-    center: hotel
-  });
-  var marker = new google.maps.Marker({
-    position: hotel,
-    map: map
-  });
-}
 
 function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
