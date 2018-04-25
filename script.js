979ece80decdd870d4234ff47c5d82dca3710018
Bazdaka <36430295+Bazdaka@users.noreply.github.com>
2018-04-25 13:57:26 +0930

    changes to font, spacing in some instances and making the manage bookings page nicer

--- script.js
+++ script.js
@@ -112,8 +112,21 @@ console.log("hello there")
 }
 
 function checkHotel1(){
-    console.log("hello there")
-    document.getElementById('pop1').style.display='block'; 
+    fade('pop1');
+}
+
+function fade(sender) {
+    var element = document.getElementById(sender);
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
 }
 
 function checkHotel2(){
